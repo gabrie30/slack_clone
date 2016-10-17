7278713cf960509548cf7472a8fa44c4808cb33b
Jay Gabriels <jgabriels30@gmail.com>
2016-10-17 08:18:54 -0700

    change chat room message ordering and action cable config

--- app/jobs/message_relay_job.rb
+++ app/jobs/message_relay_job.rb
@@ -4,7 +4,7 @@ class MessageRelayJob < ApplicationJob
   def perform(*args)
     # Do something later
     ActionCable.server.broadcast "chatrooms:#{message.chatroom.id}", {
-      message: MessageController.render(message),
+      message: MessagesController.render(message),
       chatroom_id: message.chatroom.id
     }
 
